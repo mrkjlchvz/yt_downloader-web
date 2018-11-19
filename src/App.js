2480db6d294c1d7a3a7a15999fc5ec4d329d63cb
Mark Chavez <markchav3z@gmail.com>
2018-11-19 10:23:27 +0800

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import YTSearch from 'youtube-api-search';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { term: '', videos: [] }
+    this.onInputChange = this.onInputChange.bind(this)
+    this.searchVideos = this.searchVideos.bind(this)
+  }
+
+  onInputChange(e) {
+    this.setState({ term: e.target.value })
+  }
+
+  searchVideos(e) {
+    let opts = {
+      key: process.env.REACT_APP_YT_API_KEY, 
+      term: this.state.term,
+    }
+
+    YTSearch(opts, (videos) => {
+      this.setState({ videos })
+      console.log(videos)
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='container'>
+        <input type='text' placeholder='e.g. cat videos' className='border border-2 border-black p-2' onChange={this.onInputChange} />
+        <button className='bg-blue text-white p-2' onClick={this.searchVideos}>Search</button>
+
+        <div className='videos'>
+          <span> hello </span>
+        </div>
       </div>
     );
   }
