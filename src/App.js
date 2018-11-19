2ba5538305c4f9f35df89a5307c57c86edc470a2
Mark Chavez <markchav3z@gmail.com>
2018-11-19 12:55:42 +0800

    Do not assign default search term

--- src/App.js
+++ src/App.js
@@ -4,47 +4,11 @@ import SearchForm from './components/SearchForm';
 import VideoList from './components/VideoList';
 import YTSearch from 'youtube-api-search';
 
-const VIDEOS = [
-  {
-    "kind": "youtube#searchResult",
-    "etag": "\"XI7nbFXulYBIpL0ayR_gDh3eu1k/PX_a6fYQGk1o9JCS61xMK6AEKSQ\"",
-    "id": {
-      "kind": "youtube#video",
-      "videoId": "Pk86yykcY34"
-    },
-    "snippet": {
-      "publishedAt": "2018-11-18T02:59:19.000Z",
-      "channelId": "UCxrVzY9dkWKHeKh4eE1jm5Q",
-      "title": "Luffy Tries To Predict Katakuri Attacks - One Piece 862",
-      "description": "Scene is From One Piece Ep 862 One Piece by Eiichiro Oda and Toei Animation Disclaimer : All Rights Goes To Toei Animation One Piece Episode 862 HD ...",
-      "thumbnails": {
-        "default": {
-          "url": "https://i.ytimg.com/vi/Pk86yykcY34/default.jpg",
-          "width": 120,
-          "height": 90
-        },
-        "medium": {
-          "url": "https://i.ytimg.com/vi/Pk86yykcY34/mqdefault.jpg",
-          "width": 320,
-          "height": 180
-        },
-        "high": {
-          "url": "https://i.ytimg.com/vi/Pk86yykcY34/hqdefault.jpg",
-          "width": 480,
-          "height": 360
-        }
-      },
-      "channelTitle": "MarcoThePhoenix",
-      "liveBroadcastContent": "none"
-    }
-  }
-]
-
 class App extends Component {
 
   constructor(props) {
     super(props)
-    this.state = { term: '', videos: VIDEOS }
+    this.state = { term: '', videos: [] }
     this.setSearchTerm = this.setSearchTerm.bind(this)
     this.searchVideos = this.searchVideos.bind(this)
   }
